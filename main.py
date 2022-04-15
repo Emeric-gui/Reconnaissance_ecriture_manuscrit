08828ac68b3fa4f2694a174d466f6e3d2f17a78c
Emeric Guichet <61002169+Emeric-gui@users.noreply.github.com>
2022-04-14 20:14:44 -0400

    Update main.py
    refonte du main

--- main.py
+++ main.py
@@ -1,52 +1,35 @@
-import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
-from extra_keras_datasets import emnist
+import cv2
+import os
+import sys
+from traitement import Traitement
 
-(input_train, target_train), (input_test, target_test) = emnist.load_data(type='byclass')
-num_classes = 62
-input_shape = (28, 28, 1)
-(x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+"""On commence par récupérer le repertoire d'images
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images size is (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+    Il contiendra les différentes sous images
+
+    Dans les arguments du programme entrez le chemin relatif du dossier où sont stockées les images.
+
+"""
+
+args = sys.argv
+repertoireStockImages = args[1]
 
 
-# Convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+from interface import Fenetre
 
+fenetre = Fenetre()
+fenetre.activateWindow()
 
-model = keras.Sequential(
-    [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation="relu"),
-        #layers.Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation="relu"),
-        #layers.Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation="relu"),
-        #layers.Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
-model.summary()
-
-
-batch_size = 128
-epochs = 15
-
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
+#
+# # Repertoire qui contient toutes les images
+# liste_images = os.listdir(repertoireStockImages)
+# i = 1
+#
+# # Parcours de toutes les images et traitement sur chacune d'elles
+# for image in liste_images:
+#     print("Image : {0}".format(image))
+#     cheminImage = repertoireStockImages + "/" + image
+#
+#     traitement = Traitement(cheminImage)
+#     traitement.pretraitement()
+#     traitement.show_image()
